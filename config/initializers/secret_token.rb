c1460cd7fa16fbab4ee3166571247df3559280ad
yasemincidem <yasemincidem@gmail.com>
2014-02-01 11:34:33 +0200

    Procet created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ab2014::Application.config.secret_key_base = '4e63c0a0dc3e21d07335115b03dab4a5ed1b4281180bfcd55bafb556ba4b428b56cb1a7239c51b7c6af8fd574b1d5239218db68bacdffc821da3a1d99be0d8aa'
